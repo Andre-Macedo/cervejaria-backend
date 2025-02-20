f0dd8cf082d58854a38ba60dcc1de0043050bdf7
Andre-Macedo <andrluis@protonmail.com>
2025-02-20 15:43:33 -0300

    add blade components

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,14 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cervejaria Artesanal</title>
+    <title>Cervejaria Dionisio</title>
     @livewireStyles
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body class="bg-gray-100">
-<div class="container mx-auto p-4">
-    {{ $slot }}
-</div>
+    <x-sections.navbar/>
+    <main class="container mx-auto p-4">
+        @yield('content')
+    </main>
+    <x-sections.footer />
     @livewireScripts
 </body>
 </html>
