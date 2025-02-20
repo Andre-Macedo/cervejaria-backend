975eeba6a825c26162dc72f68aa477f39dbff8f9
Andre-Macedo <andrluis@protonmail.com>
2025-02-19 23:48:05 -0300

    add Livewire and creates some Livewire components

---
+++ resources/views/livewire/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cervejaria Artesanal</title>
+    @livewireStyles
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-100">
+<div class="container mx-auto p-4">
+    {{ $slot }}
+</div>
+    @livewireScripts
+</body>
+</html>
