1406385ec48dd1fbbaaed1baf9efeecd3102dc69
Andre-Macedo <andrluis@protonmail.com>
2025-06-05 15:59:23 -0300

    adjust apis 3

--- routes/api.php
+++ routes/api.php
@@ -85,6 +85,7 @@ Route::get('/scores', function (Request $request) {
         ->take($limit) // Apply limit
         ->get()
         ->map(function ($score, $index) use ($offset) {
+
             return [
                 'player_id' => $score->player_id,
                 'player_name' => $score->player_name,
